d4da02cfc7363873d89bdd035cf2d4de2235c33a
Wee-Bee2019 <mz1100dvvc@gmail.com>
2021-05-09 12:30:27 +0300

    feat: add Caesar cipher calculation function

--- Caesar-cipher-CLI-tool/index.js
+++ Caesar-cipher-CLI-tool/index.js
@@ -31,3 +31,58 @@ if (options.input) console.log(`Input file name: ${options.input}`)
 if (options.output) console.log(`Output file name: ${options.output}`)
 if (options.action) console.log(`Action type: ${options.action}`)
 
+// Функции
+function caesarCipher(str, range, action) {  
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    let result = []
+  
+    // Decoding action condition
+    if (action === 'decode') {
+      range *= -1
+    }
+  
+    // If range is more than alphabet's length
+    if (Math.abs(range) >= alphabet.length) {
+      if (range < 0) {
+        range += alphabet.length 
+      } else {
+        range -= alphabet.length
+      }
+    }
+  
+    // calculating
+    str.split('').forEach((el) => {
+      if (alphabet.includes(el) || alphabet.includes(el.toLowerCase())) {
+        let index = alphabet.indexOf(el.toLowerCase())
+        let isUpperCase
+  
+        // UpperCase condition
+        if (el == el.toUpperCase()) {
+          isUpperCase = true
+        }
+  
+        // shift calculating
+        el = () => {
+          if (index + range >= alphabet.length) {
+            return alphabet[alphabet.length - (index + range)]
+          }
+          // if negative shift handling
+          if (index + range < 0) {
+            return alphabet[alphabet.length + (range + index)]
+          }
+          return alphabet[index + range]
+        }
+  
+        // return UpperCase, if it was 
+        if (isUpperCase) {
+          result.push(el().toUpperCase())
+        } else {
+          result.push(el())
+        }
+      } else {
+        result.push(el)
+      }
+    })
+  
+    return result.join('')
+  }
